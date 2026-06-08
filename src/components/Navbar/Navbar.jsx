7c26ae478fb7962c47c82bbc34ac3dc7a6e0d10c
SUGANTHAN23-STACKLY <suganthans@thestackly.com>
2026-06-08 14:45:34 +0530

    Update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Menu, X } from "lucide-react";
 
 function Navbar() {
   const navigate = useNavigate();
